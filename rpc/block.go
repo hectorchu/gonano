155bf26ab63d125f5611c3655b5553ceb8e58936
Hector Chu <hectorchu@gmail.com>
2020-11-06 16:20:12 +0000

    Rearrange

--- rpc/block.go
+++ rpc/block.go
@@ -2,24 +2,6 @@ package rpc
 
 import "encoding/hex"
 
-// BlockCount reports the number of blocks in the ledger and unchecked synchronizing blocks.
-func (c *Client) BlockCount() (cemented, count, unchecked uint64, err error) {
-	resp, err := c.send(map[string]interface{}{"action": "block_count"})
-	if err != nil {
-		return
-	}
-	if cemented, err = toUint(resp["cemented"]); err != nil {
-		return
-	}
-	if count, err = toUint(resp["count"]); err != nil {
-		return
-	}
-	if unchecked, err = toUint(resp["unchecked"]); err != nil {
-		return
-	}
-	return
-}
-
 // BlockAccount returns the account containing block.
 func (c *Client) BlockAccount(hash []byte) (account string, err error) {
 	resp, err := c.send(map[string]interface{}{"action": "block_account", "hash": hex.EncodeToString(hash)})
@@ -40,4 +22,22 @@ func (c *Client) BlockConfirm(hash []byte) (started bool, err error) {
 		return
 	}
 	return started2 == 1, nil
+}
+
+// BlockCount reports the number of blocks in the ledger and unchecked synchronizing blocks.
+func (c *Client) BlockCount() (cemented, count, unchecked uint64, err error) {
+	resp, err := c.send(map[string]interface{}{"action": "block_count"})
+	if err != nil {
+		return
+	}
+	if cemented, err = toUint(resp["cemented"]); err != nil {
+		return
+	}
+	if count, err = toUint(resp["count"]); err != nil {
+		return
+	}
+	if unchecked, err = toUint(resp["unchecked"]); err != nil {
+		return
+	}
+	return
 }
--- rpc/block_test.go
+++ rpc/block_test.go
@@ -8,17 +8,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestBlockAccount(t *testing.T) {
+	hash, _ := hex.DecodeString("8C1B5D4BBE27F05C7A888D1E691A07C550A81AFEE16D913EE21E1093888B82FD")
+	account, err := getClient().BlockAccount(hash)
+	require.Nil(t, err)
+	assert.Equal(t, testAccount, account)
+}
+
 func TestBlockCount(t *testing.T) {
 	cemented, count, unchecked, err := getClient().BlockCount()
 	require.Nil(t, err)
 	assert.Greater(t, cemented, uint64(50000000))
 	assert.Greater(t, count, uint64(50000000))
 	assert.Less(t, unchecked, uint64(100000))
-}
-
-func TestBlockAccount(t *testing.T) {
-	hash, _ := hex.DecodeString("8C1B5D4BBE27F05C7A888D1E691A07C550A81AFEE16D913EE21E1093888B82FD")
-	account, err := getClient().BlockAccount(hash)
-	require.Nil(t, err)
-	assert.Equal(t, testAccount, account)
 }
