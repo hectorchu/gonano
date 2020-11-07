74286a653c4c1319c9288d83e0a4ee4907d3635b
Hector Chu <hectorchu@gmail.com>
2020-11-07 20:41:14 +0000

    Add active_difficulty

---
+++ rpc/misc_test.go
@@ -0,0 +1,24 @@
+package rpc_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestActiveDifficulty(t *testing.T) {
+	multiplier, networkCurrent, networkMinimum,
+		networkReceiveCurrent, networkReceiveMinimum,
+		difficultyTrend, err := getClient().ActiveDifficulty()
+	require.Nil(t, err)
+	assert.GreaterOrEqual(t, multiplier, 1.0)
+	assert.Len(t, networkCurrent, 8)
+	assert.Len(t, networkMinimum, 8)
+	assert.Len(t, networkReceiveCurrent, 8)
+	assert.Len(t, networkReceiveMinimum, 8)
+	assert.Len(t, difficultyTrend, 20)
+	for _, multiplier := range difficultyTrend {
+		assert.GreaterOrEqual(t, multiplier, 1.0)
+	}
+}
