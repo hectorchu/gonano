4bfc280f72c61d408c33a2b973caf11688af6d5b
Hector Chu <hectorchu@gmail.com>
2020-11-08 13:45:03 +0000

    Remove block_hash test

--- rpc/block_test.go
+++ rpc/block_test.go
@@ -68,22 +68,6 @@ func TestBlockCreate(t *testing.T) {
 	assertEqualBytes(t, "cab7404f0b5449d0", block.Work)
 }
 
-func TestBlockHash(t *testing.T) {
-	hash, err := getClient().BlockHash(&rpc.Block{
-		Type:           "state",
-		Account:        "nano_3qgmh14nwztqw4wmcdzy4xpqeejey68chx6nciczwn9abji7ihhum9qtpmdr",
-		Previous:       hexString("F47B23107E5F34B2CE06F562B5C435DF72A533251CB414C51B2B62A8F63A00E4"),
-		Representative: "nano_1hza3f7wiiqa7ig3jczyxj5yo86yegcmqk3criaz838j91sxcckpfhbhhra1",
-		Balance:        strToRaw("1000000000000000000000"),
-		Link:           hexString("19D3D919475DEED4696B5D13018151D1AF88B2BD3BCFF048B45031C1F36D1858"),
-		LinkAsAccount:  "nano_18gmu6engqhgtjnppqam181o5nfhj4sdtgyhy36dan3jr9spt84rzwmktafc",
-		Signature:      hexString("3BFBA64A775550E6D49DF1EB8EEC2136DCD74F090E2ED658FBD9E80F17CB1C9F9F7BDE2B93D95558EC2F277FFF15FD11E6E2162A1714731B743D1E941FA4560A"),
-		Work:           hexString("cab7404f0b5449d0"),
-	})
-	require.Nil(t, err)
-	assertEqualBytes(t, "FF0144381CFF0B2C079A115E7ADA7E96F43FD219446E7524C48D1CC9900C4F17", hash)
-}
-
 const testBlockInfoHash = "8C1B5D4BBE27F05C7A888D1E691A07C550A81AFEE16D913EE21E1093888B82FD"
 
 func testBlockInfo(t *testing.T, info *rpc.BlockInfo) {
