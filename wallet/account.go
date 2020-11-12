6e954269e10fd49fb118070d9c1c84be95852928
Hector Chu <hectorchu@gmail.com>
2020-11-12 17:25:45 +0000

    Reorganise

---
+++ wallet/account.go
@@ -0,0 +1,127 @@
+package wallet
+
+import (
+	"encoding/hex"
+	"errors"
+	"math/big"
+
+	"github.com/hectorchu/gonano/rpc"
+	"github.com/hectorchu/gonano/wallet/ed25519"
+	"golang.org/x/crypto/blake2b"
+)
+
+// Account represents a wallet account.
+type Account struct {
+	w           *Wallet
+	key, pubkey []byte
+	address     string
+}
+
+// Address returns the address of the account.
+func (a *Account) Address() string {
+	return a.address
+}
+
+// Send sends an amount to an account.
+func (a *Account) Send(account string, amount *big.Int) (hash rpc.BlockHash, err error) {
+	link, err := addressToPubkey(account)
+	if err != nil {
+		return
+	}
+	info, err := a.w.RPC.AccountInfo(a.address)
+	if err != nil {
+		return
+	}
+	info.Balance.Sub(&info.Balance.Int, amount)
+	if info.Balance.Cmp(&big.Int{}) < 0 {
+		err = errors.New("insufficient funds")
+		return
+	}
+	block := &rpc.Block{
+		Type:           "state",
+		Account:        a.address,
+		Previous:       info.Frontier,
+		Representative: info.Representative,
+		Balance:        info.Balance,
+		Link:           link,
+		LinkAsAccount:  account,
+	}
+	if err = a.sign(block); err != nil {
+		return
+	}
+	if block.Work, _, _, err = a.w.RPCWork.WorkGenerate(info.Frontier); err != nil {
+		return
+	}
+	return a.w.RPC.Process(block, "send")
+}
+
+// ReceivePendings pockets all pending amounts.
+func (a *Account) ReceivePendings() (err error) {
+	pendings, err := a.w.RPC.AccountsPending([]string{a.address}, -1)
+	if err != nil {
+		return
+	}
+	return a.receivePendings(pendings[a.address])
+}
+
+func (a *Account) receivePendings(pendings rpc.HashToPendingMap) (err error) {
+	if len(pendings) == 0 {
+		return
+	}
+	info, err := a.w.RPC.AccountInfo(a.address)
+	if err != nil {
+		info.Balance = &rpc.RawAmount{}
+		info.Representative = "nano_1stofnrxuz3cai7ze75o174bpm7scwj9jn3nxsn8ntzg784jf1gzn1jjdkou"
+	}
+	for hash, pending := range pendings {
+		link, err := hex.DecodeString(hash)
+		if err != nil {
+			return err
+		}
+		workHash := info.Frontier
+		if info.Frontier == nil {
+			info.Frontier = make(rpc.BlockHash, 32)
+			workHash = a.pubkey
+		}
+		info.Balance.Add(&info.Balance.Int, &pending.Amount.Int)
+		block := &rpc.Block{
+			Type:           "state",
+			Account:        a.address,
+			Previous:       info.Frontier,
+			Representative: info.Representative,
+			Balance:        info.Balance,
+			Link:           link,
+			LinkAsAccount:  pending.Source,
+		}
+		if err = a.sign(block); err != nil {
+			return err
+		}
+		if block.Work, _, _, err = a.w.RPCWork.WorkGenerate(workHash); err != nil {
+			return err
+		}
+		if info.Frontier, err = a.w.RPC.Process(block, "receive"); err != nil {
+			return err
+		}
+	}
+	return
+}
+
+func (a *Account) sign(block *rpc.Block) (err error) {
+	hash, err := blake2b.New256(nil)
+	if err != nil {
+		return
+	}
+	hash.Write(make([]byte, 31))
+	hash.Write([]byte{6})
+	hash.Write(a.pubkey)
+	hash.Write(block.Previous)
+	pubkey, err := addressToPubkey(block.Representative)
+	if err != nil {
+		return
+	}
+	hash.Write(pubkey)
+	hash.Write(block.Balance.FillBytes(make([]byte, 16)))
+	hash.Write(block.Link)
+	block.Signature = ed25519.Sign(a.key, hash.Sum(nil))
+	return
+}
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,90 +1,49 @@
 package wallet
 
 import (
-	"encoding/hex"
-	"errors"
-	"math/big"
-
 	"github.com/hectorchu/gonano/rpc"
-	"github.com/hectorchu/gonano/wallet/ed25519"
-	"golang.org/x/crypto/blake2b"
 )
 
 // Wallet represents a wallet.
 type Wallet struct {
 	seed         []byte
 	index        uint32
-	key          map[string][]byte
+	accounts     map[string]*Account
 	RPC, RPCWork rpc.Client
 }
 
 // NewWallet creates a new wallet.
-func NewWallet(seed []byte) (wallet *Wallet, err error) {
-	wallet = &Wallet{
-		seed:    seed,
-		key:     make(map[string][]byte),
-		RPC:     rpc.Client{URL: "https://mynano.ninja/api/node"},
-		RPCWork: rpc.Client{URL: "http://[::1]:7076"},
+func NewWallet(seed []byte) *Wallet {
+	return &Wallet{
+		seed:     seed,
+		accounts: make(map[string]*Account),
+		RPC:      rpc.Client{URL: "https://mynano.ninja/api/node"},
+		RPCWork:  rpc.Client{URL: "http://[::1]:7076"},
 	}
-	return
 }
 
 // NewAccount creates a new account.
-func (w *Wallet) NewAccount() (address string, err error) {
+func (w *Wallet) NewAccount() (a *Account, err error) {
 	key, err := deriveKey(w.seed, w.index)
 	if err != nil {
 		return
 	}
-	pubkey, key, err := deriveKeypair(key)
-	if err != nil {
+	a = &Account{w: w}
+	if a.pubkey, a.key, err = deriveKeypair(key); err != nil {
 		return
 	}
-	address, err = deriveAddress(pubkey)
-	if err != nil {
+	if a.address, err = deriveAddress(a.pubkey); err != nil {
 		return
 	}
-	w.key[address] = key
+	w.accounts[a.address] = a
 	w.index++
 	return
 }
 
-// Send sends an amount from an account.
-func (w *Wallet) Send(account, destAccount string, amount *big.Int) (hash rpc.BlockHash, err error) {
-	info, err := w.RPC.AccountInfo(account)
-	if err != nil {
-		return
-	}
-	link, err := addressToPubkey(destAccount)
-	if err != nil {
-		return
-	}
-	info.Balance.Sub(&info.Balance.Int, amount)
-	if info.Balance.Cmp(&big.Int{}) < 0 {
-		err = errors.New("insufficient funds")
-		return
-	}
-	block := &rpc.Block{
-		Type:           "state",
-		Account:        account,
-		Previous:       info.Frontier,
-		Representative: info.Representative,
-		Balance:        info.Balance,
-		Link:           link,
-		LinkAsAccount:  destAccount,
-	}
-	if err = w.sign(block); err != nil {
-		return
-	}
-	if block.Work, _, _, err = w.RPCWork.WorkGenerate(info.Frontier); err != nil {
-		return
-	}
-	return w.RPC.Process(block, "send")
-}
-
-// ReceivePendings receives all pending amounts.
+// ReceivePendings pockets all pending amounts.
 func (w *Wallet) ReceivePendings() (err error) {
-	accounts := make([]string, 0, len(w.key))
-	for address := range w.key {
+	accounts := make([]string, 0, len(w.accounts))
+	for address := range w.accounts {
 		accounts = append(accounts, address)
 	}
 	pendings, err := w.RPC.AccountsPending(accounts, -1)
@@ -92,80 +51,9 @@ func (w *Wallet) ReceivePendings() (err error) {
 		return
 	}
 	for account, pendings := range pendings {
-		if len(pendings) == 0 {
-			continue
-		}
-		info, err := w.RPC.AccountInfo(account)
-		if err != nil {
-			info.Balance = &rpc.RawAmount{}
-			info.Representative = "nano_1stofnrxuz3cai7ze75o174bpm7scwj9jn3nxsn8ntzg784jf1gzn1jjdkou"
-		}
-		for hash, pending := range pendings {
-			link, err := hex.DecodeString(hash)
-			if err != nil {
-				return err
-			}
-			info.Balance.Add(&info.Balance.Int, &pending.Amount.Int)
-			if info.Frontier, err = w.receive(
-				account, info.Representative, pending.Source,
-				info.Balance, info.Frontier, link,
-			); err != nil {
-				return err
-			}
-		}
-	}
-	return
-}
-
-func (w *Wallet) receive(
-	account, representative, sourceAccount string,
-	balance *rpc.RawAmount, previous, link rpc.BlockHash,
-) (hash rpc.BlockHash, err error) {
-	workHash := previous
-	if previous == nil {
-		previous = make(rpc.BlockHash, 32)
-		if workHash, err = addressToPubkey(account); err != nil {
+		if err = w.accounts[account].receivePendings(pendings); err != nil {
 			return
 		}
 	}
-	block := &rpc.Block{
-		Type:           "state",
-		Account:        account,
-		Previous:       previous,
-		Representative: representative,
-		Balance:        balance,
-		Link:           link,
-		LinkAsAccount:  sourceAccount,
-	}
-	if err = w.sign(block); err != nil {
-		return
-	}
-	if block.Work, _, _, err = w.RPCWork.WorkGenerate(workHash); err != nil {
-		return
-	}
-	return w.RPC.Process(block, "receive")
-}
-
-func (w *Wallet) sign(block *rpc.Block) (err error) {
-	hash, err := blake2b.New256(nil)
-	if err != nil {
-		return
-	}
-	hash.Write(make([]byte, 31))
-	hash.Write([]byte{6})
-	pubkey, err := addressToPubkey(block.Account)
-	if err != nil {
-		return
-	}
-	hash.Write(pubkey)
-	hash.Write(block.Previous)
-	pubkey, err = addressToPubkey(block.Representative)
-	if err != nil {
-		return
-	}
-	hash.Write(pubkey)
-	hash.Write(block.Balance.FillBytes(make([]byte, 16)))
-	hash.Write(block.Link)
-	block.Signature = ed25519.Sign(w.key[block.Account], hash.Sum(nil))
 	return
 }
