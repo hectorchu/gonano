0ffb9aa8547013eb16b4495c8e8ac1f469b97913
Hector Chu <hectorchu@gmail.com>
2020-11-17 19:37:09 +0000

    Fix receive command

--- cmd/receive.go
+++ cmd/receive.go
@@ -8,32 +8,18 @@ var receiveCmd = &cobra.Command{
 	Use:   "receive",
 	Short: "Receive all pending amounts for a wallet or account",
 	Run: func(cmd *cobra.Command, args []string) {
-		if walletIndex < 0 {
-			checkWalletAccount()
-			for i, wi := range wallets {
-				if _, ok := wi.Accounts[walletAccount]; ok {
-					walletIndex = i
-					break
-				}
+		if walletAccount == "" {
+			checkWalletIndex()
+			wi := wallets[walletIndex]
+			wi.init()
+			for _, index := range wi.Accounts {
+				_, err := wi.w.NewAccount(&index)
+				fatalIf(err)
 			}
-			if walletIndex < 0 {
-				fatal("account not found in any wallet")
-			}
-		}
-		checkWalletIndex()
-		wi := wallets[walletIndex]
-		wi.init()
-		if walletAccount != "" {
-			index, ok := wi.Accounts[walletAccount]
-			if !ok {
-				fatal("account not found in the specified wallet")
-			}
-			a, err := wi.w.NewAccount(&index)
-			fatalIf(err)
-			err = a.ReceivePendings()
+			err := wi.w.ReceivePendings()
 			fatalIf(err)
 		} else {
-			err := wi.w.ReceivePendings()
+			err := getAccount().ReceivePendings()
 			fatalIf(err)
 		}
 	},
